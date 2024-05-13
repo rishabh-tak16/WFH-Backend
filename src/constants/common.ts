014017cb36b6aa26617a67b4432fc50357f166b9
rishabh_tak16 <rishabhtak16@gmail.com>
2024-05-13 13:48:47 +0530

    deleted files

--- src/constants/common.ts
+++
@@ -1,2 +0,0 @@
-export const MONGO_URL : string = 'mongodb+srv://rishabhtak16:L2shFKSbiEVFNAx9@backenddb.cvqwp5w.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB'
-export const SECRET_KEY : string = 'MY_KEY'
--- src/dao/SystemUser.dao.ts
+++
@@ -1,9 +0,0 @@
-import  { SystemUser,ISystemUser } from '../models/systemUser.model';
-
-export const createUser = async (userData: ISystemUser) => {
-    return await SystemUser.create(userData);
-};
-
-export const getUserByEmail = async (email: string) => {
-    return await SystemUser.findOne({ email });
-};
